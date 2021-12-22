d403fbc6b008118db833b7b8dad4fbbb783656c1
E190449 <r.hendrice@student.helmo.com>
2021-12-22 12:11:58 +0100

    Editer scan facture a faire

---
+++ inc/header.inc.php
@@ -0,0 +1,17 @@
+<header>
+    <a href="myGroups.php" class="nodebt"><img id="logo" src="img/icon.png" alt="Logo No Debt"/>No D€bt</a>
+    <nav class="website-nav">
+        <ul class="nav-links">
+            <li><a href="myGroups.php">Mes groupes</a></li>
+            <li><a href="createGroup.php">Créer un groupe</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <nav class="profile-nav">
+        <span>Machin Bidule</span>
+        <ul class="nav-links">
+            <li><a href="editProfile.php">Modifier le profil</a></li>
+            <li><a href="index.php">Se déconnecter</a></li>
+        </ul>
+    </nav>
+</header>
